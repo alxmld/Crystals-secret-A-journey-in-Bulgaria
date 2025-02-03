78f5fb5cda374fbf7e42dbc55931d4f2a21f8961
alxmld <mladenovaaleksandra7@gmail.com>
2025-02-03 14:11:43 +0200

    updated book, button assign fixes

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    [SerializeField] private BillboardType billboardType;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private float hoverDistance = 5f;  // Distance at which the sprite will appear
+    [SerializeField] private GameObject targetObject;  // The game object the sprite should appear over
+
+    public enum BillboardType { LookAtCamera, CameraForward };
+
+    private void Start()
+    {
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();  // Get the SpriteRenderer if not set
+        }
+    }
+
+    void Update()
+    {
+        // Check if the player is within hover distance and is hovering over the target object
+        if (targetObject != null && Vector3.Distance(targetObject.transform.position, Camera.main.transform.position) <= hoverDistance)
+        {
+            // Perform a raycast to detect if the player is hovering over the target object
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == targetObject)
+            {
+                // If player is hovering over the target object, show the sprite and make it follow the camera
+                spriteRenderer.enabled = true;
+
+                // Make sprite face the camera
+                switch (billboardType)
+                {
+                    case BillboardType.LookAtCamera:
+                        transform.LookAt(Camera.main.transform.position, Vector3.up);
+                        break;
+                    case BillboardType.CameraForward:
+                        transform.forward = Camera.main.transform.forward;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                // If the player is not hovering over the target object, hide the sprite
+                spriteRenderer.enabled = false;
+            }
+        }
+        else
+        {
+            // If player is not close enough, hide the sprite
+            spriteRenderer.enabled = false;
+        }
+    }
+}
