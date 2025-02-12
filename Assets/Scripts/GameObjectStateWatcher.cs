8465bfede3d8bdfe0f9a59018ba55399c2b14f1e
alxmld <mladenovaaleksandra7@gmail.com>
2025-02-12 12:02:03 +0200

    changed var names for easier use of

--- Assets/Scripts/GameObjectStateWatcher.cs
+++ Assets/Scripts/GameObjectStateWatcher.cs
@@ -3,23 +3,23 @@ using UnityEngine.UI;
 
 public class GameObjectStateWatcher : MonoBehaviour
 {
-    public GameObject firstGameObject;   // The one that should be inactive
-    public GameObject secondGameObject;  // The one that should be active
+    public GameObject prevFoundCrystalCanvas;   // The one that should be inactive
+    public GameObject room;  // The one that should be active
     public Button[] targetButtons;       // Array of buttons to enable
     public GameObject[] targetImages;    // Array of images to hide
 
-    private bool firstObjectWasActive = false;
+    private bool prevFoundCrystalCanvasWasActive = false;
 
     void Update()
     {
         // Check if the first object was active and is now inactive
-        if (firstGameObject.activeSelf)
+        if (prevFoundCrystalCanvas.activeSelf)
         {
-            firstObjectWasActive = true;
+            prevFoundCrystalCanvasWasActive = true;
         }
 
         // If the first object was active but is now inactive, and the second object is active
-        if (firstObjectWasActive && !firstGameObject.activeSelf && secondGameObject.activeSelf)
+        if (prevFoundCrystalCanvasWasActive && !prevFoundCrystalCanvas.activeSelf && room.activeSelf)
         {
             // Enable all buttons in the array
             foreach (Button btn in targetButtons)
