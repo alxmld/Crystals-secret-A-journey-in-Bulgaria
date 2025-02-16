0ec349166fd6d84ff0a51a6f80843299936cade1
alxmld <mladenovaaleksandra7@gmail.com>
2025-02-16 17:23:00 +0200

    background audio, qr codes for ar

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -1,36 +1,50 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioPlayer : MonoBehaviour
 {
-    [SerializeField] GameObject activeCave;
+    [SerializeField] private GameObject activeGameObject;
     public AudioSource bgMusicSource;
-    private bool activeCaveIsActive = false;
-    private bool isPlaying = false;
+    public float fadeDuration = 1.5f; // Time in seconds for fade effect
+    private bool isActive = false;
+    private Coroutine fadeCoroutine;
 
-    // Update is called once per frame
     void Update()
     {
-        if (activeCave.activeSelf)
+        bool shouldBeActive = activeGameObject.activeSelf;
+
+        if (shouldBeActive && !isActive)
         {
-            activeCaveIsActive = true;
-        } 
-        else
+            isActive = true;
+            if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+            fadeCoroutine = StartCoroutine(FadeAudio(bgMusicSource, 1f, fadeDuration)); // Fade in
+        }
+        else if (!shouldBeActive && isActive)
         {
-            activeCaveIsActive = false;
+            isActive = false;
+            if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+            fadeCoroutine = StartCoroutine(FadeAudio(bgMusicSource, 0f, fadeDuration)); // Fade out
+        }
+    }
+
+    private IEnumerator FadeAudio(AudioSource audioSource, float targetVolume, float duration)
+    {
+        float startVolume = audioSource.volume;
+        float time = 0f;
+
+        if (targetVolume > 0 && !audioSource.isPlaying) 
+            audioSource.Play(); // Start playing if fading in
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, targetVolume, time / duration);
+            yield return null;
         }
 
-        if (activeCaveIsActive && !isPlaying)
-        {
-            bgMusicSource.Play();
-            isPlaying = true;
-        }
-        else if (!activeCaveIsActive && isPlaying)
-        {
-            bgMusicSource.Stop();
-            activeCaveIsActive = false;
-            isPlaying = false;
-        }
+        audioSource.volume = targetVolume;
+
+        if (targetVolume == 0) 
+            audioSource.Stop(); // Stop playing if completely faded out
     }
 }
